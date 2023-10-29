a533a0a12e77530019a96b94a0caa2f803810e06
Rampratap25 <rampratap.kumar2550@gmail.com>
2023-10-29 11:09:03 +0530

    webcalculator

---
+++ Calculator/script.js
@@ -0,0 +1,17 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let displayValue = document.getElementById('display').value;
+    document.getElementById('display').value = displayValue.slice(0, -1);
+}
+
+function calculate() {
+    let result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+}
